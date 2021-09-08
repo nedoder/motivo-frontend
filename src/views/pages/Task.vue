9f781017855acbed2021a660401aa11d1dc2d56d
nedoder <nedoder89@gmail.com>
2021-09-08 16:55:39 +0200

    adding picture on attempting the challenge

--- src/views/pages/Task.vue
+++ src/views/pages/Task.vue
@@ -2,7 +2,7 @@
   <CRow class="d-flex justify-content-center">
     <CCol col="12" lg="6">
        <form @submit.prevent="submitForm">
-      <CCard v-bind:style="{height: '500px'}">
+      <CCard v-bind:style="{height: '500px', backgroundImage: `url('${this.tasks.image}')`}">
         <CCardHeader class='bg-info text-white' >
           <p>Mandatory</p>
           <h2>Challenge Id: {{ $route.params.id }}</h2>
